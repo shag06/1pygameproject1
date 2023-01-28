da685c990cce04f3a1dac6619ceed63de5ce9a28
in179 <119741781+in179@users.noreply.github.com>
2023-01-28 23:28:08 +0300

    Add files via upload

--- pause.py
+++ pause.py
@@ -1,6 +1,9 @@
+import datetime
+import sqlite3
 import pygame
 import sys
 from first_level import FirstLevel
+from Classes import Willy, Ghost
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 800))
@@ -22,12 +25,32 @@ class Button:
         screen.blit(self.image, self.rect)
         screen.blit(self.text, self.text_rect)
 
-    def checkForInput(self, position, number):
+    def checkForInput(self, position, number, *data):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
                                                                                           self.rect.bottom):
             if number == 1:
                 print("Unpausing...")
             if number == 2:
+                willy = data[0]
+                ghosts = data[1:][0]
+                date = datetime.datetime.now()
+                hp_willy = str(willy.HP)
+                hp_ghosts = " ".join([str(gh.HP) for gh in ghosts])
+                pos_willy = " ".join([str(willy.x), str(willy.y)])
+                pos_ghosts = " ".join(sum([[str(elem) for elem in gh.pos] for gh in ghosts], []))
+                db = "data\\databases\\saving.sqlite"
+                con = sqlite3.connect(db)
+                cur = con.cursor()
+                willy_id = 1
+                l = cur.execute(f"""SELECT id from save WHERE id = {willy_id}""").fetchone()
+                while l:
+                    willy_id += 1
+                    l = cur.execute(f"""SELECT id from save WHERE id = {willy_id}""").fetchone()
+                cur.execute(f"""
+                INSERT INTO save(id, date, pos_willy, pos_ghosts, hp_willy, hp_ghosts) 
+                VALUES ({willy_id}, "{date}", "{pos_willy}", "{pos_ghosts}", "{hp_willy}", "{hp_ghosts}")
+                """)
+                con.commit()
                 print("Game saved")
             if number == 3:
                 print("Main menu!")
@@ -51,26 +74,31 @@ button1 = Button(button_surface1, 500, 175, "Возобновить игру")
 button2 = Button(button_surface2, 500, 370, "Сохранить игру")
 button3 = Button(button_surface3, 500, 550, "В главное меню")
 
-def pause():
-    if __name__ == "__main__":
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    button1.checkForInput(pygame.mouse.get_pos(), 1)
-                    game = FirstLevel()
-                    game.play()
-                    button2.checkForInput(pygame.mouse.get_pos(), 2)
-                    button3.checkForInput(pygame.mouse.get_pos(), 3)
-            screen.fill("black")
-            button1.update()
-            button1.changeColor(pygame.mouse.get_pos())
-            button2.update()
-            button2.changeColor(pygame.mouse.get_pos())
-            button3.update()
-            button3.changeColor(pygame.mouse.get_pos())
-            pygame.display.update()
+def pause(willy, ghosts):
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                button1.checkForInput(pygame.mouse.get_pos(), 1)
+                # game = FirstLevel()
+                # game.play()
+                button2.checkForInput(pygame.mouse.get_pos(), 2, willy, ghosts)
+                button3.checkForInput(pygame.mouse.get_pos(), 3)
+        screen.fill("black")
+        button1.update()
+        button1.changeColor(pygame.mouse.get_pos())
+        button2.update()
+        button2.changeColor(pygame.mouse.get_pos())
+        button3.update()
+        button3.changeColor(pygame.mouse.get_pos())
+        pygame.display.update()
 
-pause()
+
+if __name__ == "__main__":
+    willy = []
+    ghosts = []
+    W = Willy(1, 1, 1)
+    G = Ghost(1, 0, 1, 1, 1)
+    pause(W, [G for i in range(4)])
